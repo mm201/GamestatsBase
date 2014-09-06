d03b9821b17cb4eafa686cc000741a17109f058a
Greg Edwards <edwardsgreg@gmail.com>
2014-09-06 18:00:42 -0400

    Initial commit, blank library project

---
+++ GamestatsBase/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamestatsBase
+{
+    public class Class1
+    {
+    }
+}
